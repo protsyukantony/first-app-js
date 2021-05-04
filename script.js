f4f146e48c5917297f0a7fb9e672119cbef6116c
Anton Protsyuk <antonprotsyuk@yandex.ru>
2021-05-04 22:25:53 +0300

    добавил функции: detectPersonalLevel() и rememberMyFilms(), а также комментарии

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 let numberOfFilms;
 
+// функция start в которой спрашиваем у пользователя кол-во просмотренных фильмов
+// проверяем на пустой ввод, отмену или ввода не числа
+
 function start() {
   numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
@@ -12,38 +15,57 @@ function start() {
 
 start();
 
+
+
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
 };
 
-for (let i = 0; i < 2; i++) {
+
+// функция rememberMyFilms, которая спрашивает 2 вопроса по 2 раза через цикл
+// если введенные данные не удовл. условию, то вопросы задаются снова
+
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
 
     const a = prompt('Один из последних просмотренных фильмов?'),
-          b = prompt('На сколько оцените его?');
+      b = prompt('На сколько оцените его?');
 
     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
+      personalMovieDB.movies[a] = b;
+      console.log('done');
     } else {
-        console.log('error');
-        i--;
+      console.log('error');
+      i--;
     }
+  }
 }
 
-if (personalMovieDB.count < 10) {
-    alert('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    alert('Вы классический зритель');
-} else if (personalMovieDB.count > 30) {
-    alert('Вы - киноман');
-} else {
-    alert('Произошла ошибка');
+rememberMyFilms();
+
+
+
+// функция detectPersonalLevel, которая анализирует значение count 
+// в зависимости от значения выводит в консоль обратную связь
+
+
+function detectPersonalLevel() {
+
+  if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель');
+  } else if (personalMovieDB.count > 30) {
+    console.log('Вы - киноман');
+  } else {
+    console.log('Произошла ошибка');
+  }
 }
+
+detectPersonalLevel();
 
 console.log(personalMovieDB);
-
-
